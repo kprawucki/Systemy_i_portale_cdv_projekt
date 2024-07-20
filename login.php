10341bc87287dd6902d3004cb0f64dde08eae8f1
kprawucki <kprawucki@edu.cdv.pl>
2024-07-20 17:38:48 +0200

    Update login.php

--- login.php
+++ login.php
@@ -6,10 +6,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-    $result = $conn->query($sql);
+    $sql = "SELECT * FROM users WHERE username=:username AND password=:password";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':password', $password);
+    $stmt->execute();
 
-    if ($result->num_rows > 0) {
+    if ($stmt->rowCount() > 0) {
         $_SESSION['loggedin'] = true;
         $_SESSION['username'] = $username;
         header("Location: dashboard.php");
