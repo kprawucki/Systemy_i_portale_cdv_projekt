4f66b992ab336827a8ff7edf99242206f90e3c26
kprawucki <kprawucki@edu.cdv.pl>
2024-07-20 15:52:54 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = ""; // Wprowadź swoje hasło do MySQL, jeśli istnieje
-$dbname = "password_manager";
+$serverName = "tcp:menadzer.database.windows.net,1433";
+$database = "password_manager";
+$username = "kprawucki@edu.cdv.pl";
+$password = "{your_password_here}";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    // Tworzenie połączenia za pomocą PDO
+    $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
+    // Ustawienie trybu błędów PDO na wyjątki
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Błąd połączenia z bazą danych: " . $e->getMessage();
+    die();
 }
 ?>
