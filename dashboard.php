04862aaf6dc86859aeb8fb4629b5446abe712b8f
kprawucki <kprawucki@edu.cdv.pl>
2024-07-20 15:53:40 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -7,19 +7,23 @@ if (!isset($_SESSION['loggedin'])) {
 
 include 'db.php';
 
-$sql = "SELECT * FROM passwords";
-$result = $conn->query($sql);
+try {
+    $sql = "SELECT * FROM passwords";
+    $stmt = $conn->query($sql);
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Błąd: " . $e->getMessage();
+    die();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel Zarządzania Hasłami</title>
     <link rel="stylesheet" href="styles.css">
 </head>
-
 <body>
     <div class="dashboard-container">
         <h2>Panel Zarządzania Hasłami</h2>
@@ -30,23 +34,22 @@ $result = $conn->query($sql);
                 <th>Hasło</th>
                 <th>Usuń</th>
             </tr>
-            <?php while ($row = $result->fetch_assoc()): ?>
-                <tr>
-                    <td><?php echo htmlspecialchars($row['service_name']); ?></td>
-                    <td><?php echo htmlspecialchars($row['username']); ?></td>
-                    <td><?php echo htmlspecialchars($row['password']); ?></td>
-                    <td>
-                        <form action="delete_record.php" method="post" style="display:inline;">
-                            <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-                            <input type="submit" value="Usuń" class="delete-button">
-                        </form>
-                    </td>
-                </tr>
-            <?php endwhile; ?>
+            <?php foreach ($results as $row): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($row['service_name']); ?></td>
+                <td><?php echo htmlspecialchars($row['username']); ?></td>
+                <td><?php echo htmlspecialchars($row['password']); ?></td>
+                <td>
+                    <form action="delete_record.php" method="post" style="display:inline;">
+                        <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                        <input type="submit" value="Usuń" class="delete-button">
+                    </form>
+                </td>
+            </tr>
+            <?php endforeach; ?>
         </table>
         <a class="button" href="add_record.php">Dodaj rekord</a>
         <a class="button" href="logout.php">Wyloguj</a>
     </div>
 </body>
-
 </html>
