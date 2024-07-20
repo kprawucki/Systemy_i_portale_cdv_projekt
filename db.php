194b907f81ebf28241a7c7b0ad842b8bcbab5596
kprawucki <kprawucki@edu.cdv.pl>
2024-07-20 16:31:13 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $serverName = "tcp:menadzer.database.windows.net,1433";
 $database = "password_manager";
-$username = "kprawucki@edu.cdv.pl";
+$username = "kprawucki@edu.cdv.pl@menadzer";
 $password = "Placki888";
 
 try {
